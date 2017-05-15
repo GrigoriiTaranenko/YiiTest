5c3e846c538d476f76a76c16ea396b818b5020f5
grigo <grigorii.taranenko@gmail.com>
2017-05-15 16:36:43 +0300

    16:36

--- frontend/tests/unit/models/BookTest.php
+++ frontend/tests/unit/models/BookTest.php
@@ -10,32 +10,31 @@ namespace frontend\tests\unit\models;
 use Yii;
 use frontend\models\Book;
 use Codeception\Test\Unit;
-use Codeception\Specify;
 
 
 class BookTest extends Unit
 {
     use \Codeception\Specify;
     private $book;
-
     public function _before(){
-        Book::deleteAll();
-        Yii::$app->db->createCommand()->insert(Book::tableName(), [
-            'name'=>'Кубок',
-            'year'=>2005,
-            'id_book_type'=>3
-        ])->execute();
+    /*    $this->book = new Book();
+             Yii::$app->db->createCommand()->insert(Book::tableName(), [
+                 'name'=>'Кубок',
+                 'year'=>2005,
+                 'id_book_type'=>3
+             ])->execute();*/
         $this->book = new Book();
+        $this->book->name='at';
     }
 
-  /*  public function testValidationEmptyValues(){
+    /*public function testValidationEmptyValues(){
         $model=new Book();
         expect('model is not valid', $model->validate())->false();
         expect('name has error', $model->getErrors())->hasKey('name');
         expect('year has error', $model->getErrors())->hasKey('year');
         expect('book type has error', $model->getErrors())->hasKey('id_book_type');
-    }
-    public function testValidationWrongValues(){
+    }*/
+  /*  public function testValidationWrongValues(){
         $model=new Book();
         $model->name='Wrong';
         $model->year='qw1233';
@@ -45,15 +44,14 @@ class BookTest extends Unit
     }*/
     public function testValidation()
     {
-
-        $this->specify("Empty Values", function() {
+ /*       $this->specify("Empty Values", function() {
             $this->book->name = null;
             $this->assertFalse($this->book->validate());
             expect('name has error', $this->book->getErrors())->hasKey('name');
             expect('year has error', $this->book->getErrors())->hasKey('year');
             expect('book type has error', $this->book->getErrors())->hasKey('id_book_type');
-        });
-
+        });*/
+/*
         $this->specify("Wrong Values", function() {
             $this->book->name = '';
             $this->book->year='241fwfas';
@@ -69,14 +67,14 @@ class BookTest extends Unit
             $this->book->year=23;
             $this->book->id_book_type=2;
             $this->assertTrue($this->book->validate());
-        });
+        });*/
     }
     public function testSaveDB(){
-        $this->book->name='Grigo';
+  /*      $this->book->name='Grigo';
         $this->book->year=12;
-        $this->book->id_book_type=3;
-        $this->book->save();
-        $book = Book::findOne(1);
+        $this->book->id_book_type=3;*/
+//        $this->book->save();
+ //       $book = Book::findOne(1);
        // $this->assertEquals($book->id, 1, 'Hello');
     }
 }
